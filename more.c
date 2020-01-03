02d6f9e18583a0d372d3d8e173cfef98201674d9
Yash <yashgupta9724@gmail.com>
2020-01-03 21:02:00 +0530

    non echo and non canonical form of input added.

--- more.c
+++ more.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <termios.h>
+#include <unistd.h>
+
+
 
 #define LINELEN 512
-#define PAGELEN 21                // currently assuming page length in terminal is fixed
+#define PAGELEN 20                               // currently assuming page length in terminal is fixed
 
+int getch(FILE*);
 void display(FILE*); 
-int get_input();
+int get_input(FILE*);
 
 int main(int argc, char* argv[]){
     if(argc == 1){
-        printf("This is the help page.\n");
-        exit(0);
+        display(stdin);
     }
     FILE* fp;
     int i = 1;
@@ -37,30 +41,36 @@ int main(int argc, char* argv[]){
 void display(FILE* fp){
     char buf[LINELEN];
     int no_of_lines_displayed = 0;
-    while(no_of_lines_displayed < PAGELEN){
-        if(fgets(buf, LINELEN, fp)){
-            fputs(buf, stdout);
-            no_of_lines_displayed++;
-        }
-        else
-            break;
-        if(no_of_lines_displayed == PAGELEN-1){
-            int i = get_input();
-            if(i == 0)
-                break;
-            else if(i == 1)
+    FILE* fp_tty = fopen("/dev/tty","r");
+    while(fgets(buf, LINELEN, fp)){
+        fputs(buf, stdout);
+        no_of_lines_displayed++;
+        if(no_of_lines_displayed == PAGELEN){
+            int i = get_input(fp_tty);
+            if(i == 0){
+                printf("\033[1A \033[2K \033[1G");
+                exit(0);
+            }
+            else if(i == 1){
+                printf("\033[1A \033[2K \033[1G");
                 no_of_lines_displayed -= PAGELEN;
-            else if(i == 2)
+            }
+            else if(i == 2){
+                printf("\033[1A \033[2K \033[1G");
                 no_of_lines_displayed--;
-            else
+            }
+            else{
+                printf("\033[1A \033[2K \033[1G");
                 break;
+            }
         }
     }
 }
 
-int get_input(){
+int get_input(FILE* terminalStream){
     char c;
-    c = getchar();
+    printf("\033[7m--more--(%%)\033[m");
+    c = getch(terminalStream);
     if(c == 'q')
         return 0;
     else if(c ==  ' ')
@@ -70,3 +80,16 @@ int get_input(){
     return 3;
 }
 
+/* reads from keypress, doesn't echo */
+int getch(FILE* i)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getc(i);
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
